11f5f24f7897028aeaa32080cf3dc44acf9d0341
Mitul <mitulgondaliya27@gmail.com>
2024-01-12 10:32:27 +0530

    completed hooks

--- src/app/public-components/home/home.component.ts
+++ src/app/public-components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { publishFacade } from '@angular/compiler';
-import { AfterViewInit, Component, Inject, OnInit, ViewChild, inject } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, Inject, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { AuthorsComponent } from 'src/app/shared/components/authors/authors.component';
 import { AuthorModel } from 'src/app/shared/models/authors.model';
@@ -11,16 +11,33 @@ import { TestService } from 'src/app/shared/services/test.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  @ViewChild('btnCounter') btnCounter : ElementRef;
+  @ViewChild(AuthorsComponent) authComponent: AuthorsComponent;
 
   public count: number = 0;
   public test: boolean = true;
   public address: string = "India";
   public obj: AuthorModel = { id: 10, name: 'mitul'};
+
   constructor(public _testServices: TestService) { }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    console.log("Home component destroy")
+  }
 
+  ngAfterViewChecked(): void {
+    console.log(this.authComponent.count);
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.btnCounter);
+    this.btnCounter.nativeElement.innerHTML = "Button text updated";
+  }
+
+  ngOnInit(): void {
+    this.timer();
   }
 
   public counter(): void {
@@ -28,6 +45,13 @@ export class HomeComponent implements OnInit {
     this.test = !this.test;
     this.obj.id = this.count++;
     this.address = this.address + this.count;
+  }
+  
+
+  timer(): void {
+    setInterval(() => {
+      this.count++;
+    },1000) 
   }
 
 }
--- src/app/shared/components/authors/authors.component.ts
+++ src/app/shared/components/authors/authors.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, AfterContentInit, Component, ContentChild, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component, ContentChild, DoCheck, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { TestService } from '../../services/test.service';
 import { AuthorModel } from '../../models/authors.model';
 import { AuthorAddressComponent } from '../author-address/author-address.component';
@@ -8,13 +8,13 @@ import { AuthorAddressComponent } from '../author-address/author-address.compone
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.scss']
 })
-export class AuthorsComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked {
+export class AuthorsComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, OnDestroy {
 
   @ContentChild(AuthorAddressComponent) authAddress: AuthorAddressComponent;
   @Input() data: number;
   @Input() data2: boolean;
   @Input() author: AuthorModel
-
+  public count: number = 0;
   constructor() {}
   ngAfterContentInit(): void {
     console.log('After content Init ' + this.authAddress?.address)
@@ -33,6 +33,16 @@ export class AuthorsComponent implements OnInit, OnChanges, DoCheck, AfterConten
   ngOnChanges(changes: SimpleChanges): void {
       console.log(changes)
   }
+
+  ngOnDestroy(): void {
+    console.log("Authors component destroy")
+  }
+
+
+  public incCount(): void{
+    this.count++;
+  }
+
 }
 
 
