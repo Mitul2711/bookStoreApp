077d2d7a458fbbd34cddd1bbe07e961d362f656e
Mitul <mitulgondaliya27@gmail.com>
2024-01-12 16:16:30 +0530

    remove unneccessary code

--- src/app/books/components/add-book/add-book.component.ts
+++ src/app/books/components/add-book/add-book.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookService } from '../../services/book.service';
 import { BookModel } from '../../models/book.model';
 
