9728b7da86b8e1426773e87667ad4b36ee253b8a
Mitul <mitulgondaliya27@gmail.com>
2024-01-16 15:47:09 +0530

    done form validation

--- src/app/books/components/add-book/add-book.component.ts
+++ src/app/books/components/add-book/add-book.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BookService } from '../../services/book.service';
 import { BookModel } from '../../models/book.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-book',
@@ -11,23 +12,38 @@ export class AddBookComponent implements OnInit {
 
   constructor(private _bookService: BookService) {}
 
+  @ViewChild('myForms') myForms: NgForm;
+
+  public model : BookModel;
+
   ngOnInit(): void {
-    
+    this.model = new BookModel();
+    this.model.title = "";
+    this.model.author = "";
+    this.model.totalPage;
+    this.model.price = {
+      value: 100,
+      currency: ''
+    };
+    this.model.isPublished = true;
   }
 
-  saveBook(value: any): void{
-    const book: BookModel = new BookModel;
-    book.title = value.title;
-    book.author = value.author;
-    book.totalPage = value.pages;
-    book.price = {
-      currency: "INR",
-      value: value.price
-    };
-    book.isPublished = value.isPublished;
-    book.publishedOn = value.publishedOn;
-    this._bookService.addBook(book)
-    console.log(book)
+  prices: any[] = [
+    {value: 100, viewValue: '100 INR'},
+    {value: 200, viewValue: '200 INR'},
+    {value: 300, viewValue: '300 INR'},
+  ];
+
+  saveBook(): void{
+
+    if(this.myForms.valid) {
+      this._bookService.addBook(this.model);
+    }
+    else {
+      alert('Invalid Form');
+    }
+
+    
   }
   
 
